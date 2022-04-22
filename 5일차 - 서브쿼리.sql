c4d1e0cfa0987ce6f79abe54d4bdfa70ebfe0acd
Suhyang Gwon <znsl65@naver.com>
2022-04-22 22:35:19 +0900

    [update] 2022.04.22

--- 5일차 - 서브쿼리.sql
+++ 5일차 - 서브쿼리.sql
@@ -143,7 +143,7 @@ order by salary asc;
  	각열에 별칭값을 한글로 넣으시오. 
 select e.ename as "사원이름", e.eno as "사원번호", e.manager as "관리자번호", m.ename as "관리자이름"
 from employee e, employee m
-where e.manager = m.ename
+on e.manager = m.eno
 
 8. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력 하시오. 
 select e.ename, e.eno, m.ename
@@ -151,88 +151,81 @@ from employee e join employee m
 on e.manager = m.eno (+)
 order by e.eno desc;
 
---9. SELF JOIN을 사용하여 지정한 사원('SCOTT')의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. 
+9. SELF JOIN을 사용하여 지정한 사원('SCOTT')의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. 
    단, 각 열의 별칭은 이름, 부서번호, 동료로 하시오. 
 select e.ename 이름 , e.dno 부서번호, m.ename 동료
 from employee e, employee m
-where e.ename = m.ename
-and e.dno = 
+where e.eno = m.dno 
+and e.ename = 'SCOTT'
 
 
---10. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오. 
-select e.first_name, e.hire_date, m.first_name 
+10. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오. 
+select m.ename, m.hiredate
 from employees e, employees m
-where e.first_name = m.first_name
-and e.hire_date > (select hire_date from employees where first_name = 'David')
+where e.hiredate < m.hiredate
+and e.ename='WARD'
 
 
---11. SELF JOIN을 사용하여 관리자 보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자 이름 및 입사일과 함께 출력하시오. 
+11. SELF JOIN을 사용하여 관리자 보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자 이름 및 입사일과 함께 출력하시오. 
     단, 각 열의 별칭을 한글로 넣어서 출력 하시오. 
-select e.ename as 사원이름, e.hiredate as 입사날짜, e.dname as 관리자이름, m. as 관리자입사일
+select e.ename as 사원이름, e.hiredate as 입사날짜, m.ename as 관리자, m. as 관리자입사일
 from employee e, employee m
-where e.hiredate < (select 
+where e.hiredate < m.hiredate
+and e.manager = m.eno
 
 <<아래 문제는 모두 Subquery를 사용하여 문제를 푸세요.>>
 
 1. 사원번호가 7788인 사원과 담당 업무가 같은 사원을 표시(사원이름 과 담당업무) 하시오.  
 select ename, job
 from employee
-where ename = (select ename from employee where dno = 7788);
+where job = (select job from employee where eno = 7788);
 
 2. 사원번호가 7499인 사원보다 급여가 많은 사원을 표시 (사원이름 과 담당업무) 하시오. 
 select ename, job
 from employee
-where salary > (select ename from employee where dno = 7499);
+where salary > (select salary from employee where eno = 7499);
 
---3. 최소 급여를 받는 직급별로 사원의 이름, 담당 업무 및 급여를 표시 하시오(그룹 함수 사용)
+3. 최소 급여를 받는 직급별로 사원의 이름, 담당 업무 및 급여를 표시 하시오(그룹 함수 사용)
 select ename, job, salary
 from employee
-where salary < (select min(salary) from employee)
-group by job;
-
-select first_name, job_id, salary 
-from employees
-group by job_id;
-having salary < (select min(salary) from employees)
-
-select * from employees
+where salary = (select min(salary) from employee);
 
 4. 직급에서 평균 급여를 구하고, 가장 작은 직급 평균을 가진 사원 중 급여가 가장 작은 사원의 직급과 급여를 표시하시오.
-select job, min(salary)
+select job, avg(salary)
 from employee
-where salalry <= (select min(salary) from employee)
-group by rollup(job)
+group by job
+having avg(salalry) = (select min(avg(salary)) from employee group by job);
 
---5. 각 부서의 최소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.
+5. 각 부서의 최소 급여를 받는 사원의 이름, 급여, 부서번호를 표시하시오.
 select ename, salary, dno
 from employee
-where salary in (select min(salary) from employee group by dno)  
+where min(salary) in (select min(salary) from employee group by dno)  
 
 
 6. 담당 업무가 분석가(ANALYST) 인 사원보다 급여가 적으면서 업무가 분석가가 아닌 사원들을 표시 (사원번호, 이름, 담당업무, 급여) 하시오.
 select eno, ename, job, salary
 from employee
-where salary < (select salary from employee where job = 'ANALYST'
-                and job <> 'ANALYST')
+where salary < any (select salary from employee where job = 'ANALYST')
+                and job <> 'ANALYST';
 
---7. 부하직원이 없는 사원의 이름을 표시 하시오. 
+7. 부하직원이 없는 사원의 이름을 표시 하시오. 
 select ename
 from employee
-where ename = (select ename from employee where dname is null) 
+where eno no in (select manager from employee where manager is not null) 
 
---8. 부하직원이 있는 사원의 이름을 표시 하시오. 
+8. 부하직원이 있는 사원의 이름을 표시 하시오. 
 select ename
 from employee
-where ename = (select ename from employee where dname is not null) 
+where eno in (select manager from employee where manager is not null) 
 
 9. BLAKE 와 동일한 부서에 속한 사원의 이름과 입사일을 표시하는 질의를 작성하시오(단, BLAKE 는 제외). 
 select ename, hiredate
 from employee
-where dno in (select dno from employee where ename = 'BLAKE'
-              and ename <> 'BLAKE')
+where dno = (select dno from employee where ename = 'BLAKE')
+              and ename <> 'BLAKE';
 
 10. 급여가 평균보다 많은 사원들의 사원번호와 이름을 표시하되 결과를 급여에 대해서 오름 차순으로 정렬 하시오. 
-select eno, ename,salary
+select eno, ename, salary
 from employee
 where salary > (select round(avg(salary)) from employee)
 order by salary;
@@ -240,28 +233,38 @@ order by salary;
 11. 이름에 K 가 포함된 사원과 같은 부서에서 일하는 사원의 사원번호와 이름을 표시하는 질의를 작성하시오. 
 select eno, ename
 from employee
-where dno = (select dno from employee where ename like '%K%')
+where dno in (select dno from employee where ename like '%K%');
 
 --12. 부서 위치가 DALLAS 인 사원의 이름과 부서 번호 및 담당 업무를 표시하시오. 
-select ename, dno, job, loc
+select ename, dno, job
 from employee
-where loc = (select )
+where dno  = (select dno from department where loc ='DALLAS' );
 
 13. KING에게 보고하는 사원의 이름과 급여를 표시하시오. 
 select ename, salary
 from employee
-where ename = (select ename from employee where manager = 'KING')
+where manager = (select eno from employee where ename = 'KING')
 
 14. RESEARCH 부서(department 테이블)의 사원에 대한 부서번호, 사원이름 및 담당 업무를 표시 하시오. 
-
-
+select dno, ename, job
+from employee
+where dno = (select dno from department where dname = 'RESEARCH');
 
 15. 평균 급여보다 많은 급여를 받고 이름에 M이 포함된 사원과 같은 부서에서 근무하는 사원의 사원번호, 이름, 급여를 표시하시오. 
+select eno, ename, salary
+from employee
+where salary > (select avg(sal) from employee)
+                and select eno in (select dno from employee where ename like '%M%'); 
 
 16. 평균 급여가 가장 적은 업무를 찾으시오. 
+select job, avg(salary)
+from employee
+group by job
+having avg(salary) = (select min(avg(salary)) from employee group by job);
 
 17. 담당업무가 MANAGER인 사원이 소속된 부서와 동일한 부서의 사원을 표시하시오. 
-
-
+selec ename
+from employee
+where dno = (select dno from employee wherer job = 'MANAGER');
 
 
